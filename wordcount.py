49317bcec7a55bb96041e4f9f6bd19e73326a457
Angela Purcell <kalmpurcell@gmail.com>
2020-10-05 20:53:51 +0000

    defined letter_count_in_dict

--- wordcount.py
+++ wordcount.py
@@ -1,1 +1,16 @@
 # put your code here.
+ 
+def letter_count_in_dict(file):
+    ''' Defines count of each letter in dictionary'''
+    
+    nFile = open(file)
+    counted_letters = {} #empty dictionary 
+
+    for letter in nFile:
+        # if letter.isalpha() == True: 
+        counted_letters[letter] = counted_letters.get(letter, 0) + 1
+
+    return counted_letters
+
+print(letter_count_in_dict('test.txt'))
+

baec42a47ba84c918c008bd6e112d8e678d8e75c
Angela Purcell <kalmpurcell@gmail.com>
2020-10-05 18:53:02 -0700

    finished

--- wordcount.py
+++ wordcount.py
@@ -1,16 +1,22 @@
 # put your code here.
  
-def letter_count_in_dict(file):
+def word_count_in_dict(file):
     ''' Defines count of each letter in dictionary'''
     
     nFile = open(file)
-    counted_letters = {} #empty dictionary 
+    counted_words= {} #empty dictionary 
+    
+    for line in nFile: ## for line in nFile:
+        line = line.split(" ")
+        # counted_letters[letter] = counted_letters.get(letter, 0) + 1
 
-    for letter in nFile:
-        # if letter.isalpha() == True: 
-        counted_letters[letter] = counted_letters.get(letter, 0) + 1
+        for word in line:
+            if word.isalpha() == True: 
+                counted_words[word] = counted_words.get(word, 0) + 1
+    
+    return counted_words
+                
 
-    return counted_letters
+    # return counted_words
 
-print(letter_count_in_dict('test.txt'))
-
+print(word_count_in_dict('test.txt'))
